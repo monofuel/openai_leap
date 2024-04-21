1f07d0f333be65ad9cc155fcc36d35eb83115fc2
monofuel <monofuel@japura.net>
2024-04-21 00:59:45 -0400

    add serialization test

---
+++ tests/test_serialization.nim
@@ -0,0 +1,44 @@
+import openai_leap, jsony, std/[unittest, options, json]
+
+# Test serialization / deserialization of requests according to the API
+
+proc validateJsonStrings*(json1: string, json2: string) =
+  ## Compare two json strings for equivelancy
+  ## hunt down those pesky nulls or other differences
+  let
+    parsed1 = fromJson(json1)
+    parsed2 = fromJson(json2)
+  
+  if parsed1 != parsed2:
+    echo "json1: \n", parsed1.pretty
+    echo ""
+    echo "json2: \n", parsed2.pretty
+    echo ""
+    assert false
+
+
+
+suite "serialization":
+  suite "requests":
+    test "user1":
+      let str = """
+{
+  "model": "chatgpt-4",
+  "messages": [
+    {
+      "role": "system",
+      "content": "You are a helpful assistant."
+    },
+    {
+      "role": "user",
+      "content": "Hello!"
+    }
+  ]
+}
+"""
+      let req = fromJson(str, CreateChatCompletionReq)
+      assert req.model == "chatgpt-4"
+      assert req.messages.len == 2
+      let jsonStr = toJson(req)
+      echo jsonStr
+      validateJsonStrings(str, jsonStr)
