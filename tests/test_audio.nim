58173b08db309884c9acf6a3a4f480155f598f03
treeform <starplant@gmail.com>
2024-08-28 15:20:54 -0700

    Add multi part audo api and test.

---
+++ tests/test_audio.nim
@@ -0,0 +1,8 @@
+import openai_leap
+
+let openai = newOpenAiApi()
+
+doAssert openai.audioTranscriptions(
+  "whisper-1",
+  readFile("tests/data/how_many_roads.wav")
+).text == "How many roads must a man walk down?"
